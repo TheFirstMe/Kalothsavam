df4fddc751ef419e2bbecdb8c2d4c36c1b2ae6fc
Sourabh Subhod <sourabhsubhod@gmail.com>
2018-03-02 02:55:50 +0530

    final

--- app/src/main/java/com/stacktips/speechtotext/MainActivity.java
+++ app/src/main/java/com/stacktips/speechtotext/MainActivity.java
@@ -5,14 +5,14 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 import com.stacktips.speechtotext.utilities.NetworkUtils;
@@ -25,9 +25,11 @@ import org.w3c.dom.Text;
 import java.net.URL;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private static final int REQ_CODE_SPEECH_INPUT = 100;
     //private TextView mVoiceInputTv;
@@ -38,10 +40,14 @@ public class MainActivity extends AppCompatActivity {
     private TextView mEventName;
     private TextView mResultList;
     private String eventQuery;
+    private String haha;
+    private static int var;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        var=0;
 
         //mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
         mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
@@ -54,10 +60,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                mEventName.setText("");
+                mResultList.setText("");
                 startVoiceInput();
             }
         });
     }
+
 
     private void makeEventSearchQuery(String eventQuery){
         URL eventSearchUrl = NetworkUtils.buildUrl(eventQuery);
@@ -89,10 +98,10 @@ public class MainActivity extends AppCompatActivity {
 
                     String position = result.getString("position");
                     String grade = result.getString("grade");
-                    String teams = result.getString("teams");
+                    String college = result.getString("college");
                     String points = result.getString("points");
                     text+="Position:  " + position + "\n";
-                    text+="Teams:  " + teams + "\n";
+                    text+="College:  " + college + "\n";
                     text+="Points:  " + points + "\n";
                     text+="Grade:  " + grade + "\n\n\n";
                 }
@@ -100,19 +109,26 @@ public class MainActivity extends AppCompatActivity {
                     mResultList.setText(text);
             }
             else {
+                Log.i("dd",mEventName.getText().toString());
+                if(mEventName.getText().toString().equals("")) {
+                    mEventName.setText(" ");
+                    correctedEventName(haha);
+                }
                 String error = eventObject.getString("statusText");
                 mResultList.setText("");
-                mEventName.setText(error + " for " + "\""+ eventQuery + "\"");
+                    mEventName.setText(error + " for " + "\""+ haha + "\"");
+
+
+                var-=1;
             }
             //String uniName = uniObject.getString("name");
-            //String uniURL = uniObject.getString("url");
         }
         catch (JSONException e){
             e.printStackTrace();
         }
     }
 
-    /*private static int distance(String a, String b){
+    private static int distance(String a, String b){
         a = a.toLowerCase();
         b = b.toLowerCase();
         int [] costs = new int [b.length() + 1];
@@ -130,11 +146,67 @@ public class MainActivity extends AppCompatActivity {
         }
         return costs[b.length()];
     }
-    private String correctedEventName(String speechInput){
-        for (int i = 0; i < data.length; i += 2)
-            System.out.println("distance(" + data[i] + ", " + data[i+1] + ") = " + distance(data[i], data[i+1]));
-        makeEventSearchQuery(speechInput);
-    }*/
+    private int correctedEventName(String speechInput){
+        var+=1;
+        String data[] = {"Aksharaslokam","Bharathanatyam","Chenda","Chaakyarkoothu","Computer Designing","Collage","Duff muttukali",
+                "Naatakam English","Naatakam Hindi","Naatakam Kannada","Naatakam Malayalam",
+                "Etakka","Prabandharachana Arabic","Cartoon Rachana",
+                "Prabandharachana English","Prabandharachana Hindi","Prabandharachana Kannada",
+                "Prabandharachana Malayalam","Prabandharachana Sanskrit","Prabandharachana Urdu",
+                "Pullankuzhal – Pourasthyam","Ghattam","Guitar – Paschchathyam",
+                "Kaliman Prathima Nirmaanam","Keralanatanam","Ghazal - Female","Ghazal - Male",
+                "Karnataka Sangeetham – Female","Karnataka Sangeetham – Male","Kathakali","Kathaprasangam",
+                "Kaavyakeli","Lalitha Sangeetham – Female","Lalitha Sangeetham – Male","Maddhalam","Mime",
+                "Kuchipudi",
+                "Mimicry","Mohiniyattam","Mono Act","Mappilappaattu – Group","Mappilappaattu – Single","Mrudangam",
+                "Maargamkali","Naatoti Nritham – Group","Naatanpattu","Naatoti Nritham – Single – Female",
+                "Naatoti Nritham – Single – Male","Naagaswaram – Pourasthyam","Oppana","Painting – Ennachayam",
+                "Painting – Jalachayam","Photography","Pookkalam","Kavithaalaapanam Arabic",
+                "Kavithaalaapanam English","Kavithaalaapanam Hindi","Kavithaalaapanam Kannada",
+                "Kavithaalaapanam Malayalam","Kavithaalaapanam Sanskrit","Kavithaalaapanam Urdu",
+                "Kavitharachana Arabic","Kavitharachana English","Kavitharachana Hindi","Kavitharachana Kannada",
+                "Kavitharachana Malayalam","Kavitharachana Sanskrit","Kavitharachana Urdu","Parichamuttukali",
+                "Pashchaathya Sangeetham – Single","Poster Rachana","Poorakkali","Quiz","Rangoli",
+                "Sanghagaanam – Pashchathyam","Sanghagaanam – Indian","Sanghanritham – Female","Sanghanritham – Male",
+                "Skit – Malayalam","Cherukatharachana Arabic","Cherukatharachana English","Cherukatharachana Hindi",
+                "Cherukatharachana Kannada", "Cherukatharachana Malayalam","Cherukatharachana Sanskrit","Cherukatharachana Urdu",
+                "Thirakatharachana - Documentary English","Thirakatharachana - Documentary Hindi",
+                "Thirakatharachana - Documentary Malayalam", "Thirakatharachana - Kathachithram English",
+                "Thirakatharachana - Kathachithram Hindi","Thirakatharachana - Kathachithram Malayalam",
+                "Tabala","Test Event","Thullal","Thiruvaathirakali – Female", "Theruvunaatakam Malayalam",
+                "Veena – Pourasthyam","Violin – Pourasthyam","Yakshagaanam"};
+        haha= speechInput;
+        Log.i("msg1","hello");
+        for(int i=0;i < data.length; i++){
+            String b[] = data[i].split(" ");
+            if(speechInput.equalsIgnoreCase(data[i])){
+                Log.i("msg4","hellofn");
+                makeEventSearchQuery(speechInput);
+                return 0;
+            }
+            /*else if(b.length>1){
+                if(speechInput.toLowerCase().contains(b[0].toLowerCase()))
+                    makeEventSearchQuery(data[i]);
+                return;
+            }*/
+
+        }
+
+        int min = 100,min_idx=0;
+        for (int i = 0; i < data.length; i++) {
+            if(data[i].toLowerCase().charAt(0)!=speechInput.toLowerCase().charAt(0) && data[i].charAt(data[i].length()-1)!=speechInput.charAt(speechInput.length()-1) )
+                continue;
+            int d= distance(speechInput, data[i]);
+            if(d<min){
+                min=d;
+                min_idx = i;
+            }
+        }
+        haha = data[min_idx].trim();
+        makeEventSearchQuery(data[min_idx].trim());
+        Log.i("msg2",data[min_idx].trim());
+        return 0;
+    }
 
     public class EventQueryTask extends AsyncTask<URL, Void, String> {
 
@@ -195,8 +267,9 @@ public class MainActivity extends AppCompatActivity {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                    // mVoiceInputTv.setText(result.get(0));
                     //mrec.setText(result.get(0));
-                    eventQuery = result.get(0);
-                    //correctedEventName(eventQuery);
+                    Log.i("text",result.get(0));
+                    //correctedEventName(result.get(0));
+                    haha = result.get(0);
                     makeEventSearchQuery(result.get(0));
                 }
                 break;
